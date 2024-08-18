8878ec91f9acabc8267146c1008067d9d0d049eb
sebas-sala <sebastian_orko@hotmail.com>
2024-08-18 00:04:48 -0600

    refactor: Add lazy loading for images in the tags page

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
+import cron, { Patterns } from "@elysiajs/cron";
 import { Elysia } from "elysia";
 import TagsScraper from "./scraper/tags-scraper";
+import TagsService from "./tags/tags.service";
 
 const app = new Elysia()
+	.use(
+		cron({
+			name: "Scrape tags",
+			pattern: Patterns.everySenconds(10),
+			run: async () => {
+				const tagsScrapper = new TagsScraper(
+					process.env.SCRAPE_MANGAS_TAGS_URL as string,
+				);
+
+				const html = await tagsScrapper.fetchPage();
+				await tagsScrapper.parse(html);
+
+				console.log("Scraped tags");
+			},
+		}),
+	)
 	.get("/", () => "Hello, World!")
 	.get("/tags", async () => {
-		const tagsScrapper = new TagsScraper(
-			process.env.SCRAPE_MANGAS_TAGS_URL as string,
-		);
+		const tagsService = new TagsService();
 
-		const tagsHtml = await tagsScrapper.fetchPage();
-		const res = tagsScrapper.parse(tagsHtml);
+		const res = await tagsService.getTags();
 
 		return res;
 	})
